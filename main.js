9970c0d7e7e68e625afe3b98b7aa6c2482a2eb83
Uti-Favour <utifavour15@gmail.com>
2023-12-08 17:23:07 +0100

    Social Menu Dropdown Js

--- main.js
+++ main.js
@@ -7,6 +7,16 @@ const socialMenuItem2 = document.getElementById('social-menu-item-1');
 const socialMenuItem3 = document.getElementById('social-menu-item-2');
 const userMenuButton = document.getElementById('user-menu-button');
 
+
 //Mobile menu toggle
 mobileMenu.classList.toggle('block');
+
+userMenuButton.addEventListener('click' , (e) => {
+socialMenuItem1.classList.add('block');
+socialMenuItem2.classList.add('block');
+socialMenuItem3.classList.add('block');
+
+});
+
+
 
