724617fdd16cc8505cc7027d6a1df43141d54339
Uti-Favour <utifavour15@gmail.com>
2023-12-08 16:24:55 +0100

    Talwind config

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss");
+
 //NavBar Variables
 const mobileMenu = document.getElementById('mobile-menu');
 const socialMenuItem1 = document.getElementById('social-menu-item-0');
@@ -6,6 +8,5 @@ const socialMenuItem3 = document.getElementById('social-menu-item-2');
 const userMenuButton = document.getElementById('user-menu-button');
 
 //Mobile menu toggle
-mobileMenu.classList.toggle('sm:block');
-
+mobileMenu.classList.toggle('block');
 
