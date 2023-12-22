fb45034c1269827fce2ca69117b410b94625a093
Uti-Favour <utifavour15@gmail.com>
2023-12-22 15:58:54 +0100

    Wrapped texts in divs

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-document.addEventListener('DOMContentLoaded' , function(){
+document.addEventListener("DOMContentLoaded", function () {
+  //Variables
+  let navBar = document.getElementById("navBar");
+  let mobileMenu = document.getElementById("mobile-menu");
+  let hiddenToggleMenu = document.querySelector(".menu_toggle_hidden");
+  let bell = document.getElementById("bell");
 
-//Variables
-let navBar = document.getElementById('navBar');
-let mobileMenu = document.getElementById('mobile-menu');
-let hiddenToggleMenu = document.querySelector('.menu_toggle_hidden');
-let bell = document.getElementById('bell');
-
-navBar.addEventListener('click' , function(){
-hiddenToggleMenu.classList.toggle('menu_toggle_block');
-console.log('You just cliked the button');
+  function openNav(){
+    navBar.addEventListener("click", function () {
+        hiddenToggleMenu.classList.toggle("menu_toggle_block");
+        console.log("You just cliked the button");
+      });
+    
+      bell.addEventListener("click", () => {
+        document.body.style.backgroundColor = "pink";
+      });
+}
+openNav();
 });
 
-bell.addEventListener('click' , () => {
-document.body.style.backgroundColor = 'pink';
-});
-});
+
 
